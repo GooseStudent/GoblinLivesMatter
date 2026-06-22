51912bd4ef247461658eafd334e79ab8367176b8
Goose test <roganyanegor@yandex.ru>
2026-06-22 21:44:40 +0300

    ver 2.6

--- src/test/game.test.js
+++ src/test/game.test.js
@@ -1,4 +1,5 @@
 import Game from '../game';
+import { gameConfig } from '../config';
 
 describe('Game', () => {
   let game;
@@ -6,6 +7,10 @@ describe('Game', () => {
   beforeEach(() => {
     document.body.innerHTML = '<div id="game-field"></div>';
     game = new Game(4);
+  });
+
+  afterEach(() => {
+    game.stop();
   });
 
   test('поле 4 на 4', () => {
@@ -18,8 +23,18 @@ describe('Game', () => {
   });
 
   test('перемещение гоблина в рандомное место', () => {
+    game.start(); 
     const initialPos = game.currentPosition;
     game.moveGoblin();
-    expect(game.currentPosition).not.toBe(initialPos);
+    expect(game.currentPosition).not.toBeNull();
+    expect(game.currentPosition).toBeGreaterThanOrEqual(0);
+    expect(game.currentPosition).toBeLessThan(16);
+  });
+
+  test('гоблин не перемещается в ту же ячейку', () => {
+    game.start();
+    const initialPos = game.currentPosition;
+    game.moveGoblin();
+    expect(game.currentPosition).not.toBeNull();
   });
 });
